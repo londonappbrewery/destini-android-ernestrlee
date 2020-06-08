81fb36c4cc1419a4280f06afa0a5a2e099b1de6b
Ernest-Lee <ernestrlee@yahoo.com>
2020-06-07 20:10:00 -0700

    Added on click listeners to top and bottom buttons.

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,6 +2,8 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,8 +22,20 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        topButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("destini", "onClick: top button clicked");
+            }
+        });
 
 
+        bottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("destini", "onClick: bottom button clicked");
+            }
+        });
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
 
